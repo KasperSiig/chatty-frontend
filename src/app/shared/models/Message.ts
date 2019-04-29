942c460fea51cebdc4032c87d58ca9e49682526d
Oliver Sylvest Hansen <oliv9858@easv365.dk>
2019-04-29 11:08:46 +0200

    Message Model Created

---
+++ src/app/shared/models/Message.ts
@@ -0,0 +1,6 @@
+export class Message {
+  id?: string;
+  message: string;
+  time: Date;
+  sender: string;
+}
--- src/app/shared/services/message.service.ts
+++ src/app/shared/services/message.service.ts
@@ -1,4 +1,5 @@
 import { Injectable } from '@angular/core';
+import {AngularFirestore} from '@angular/fire/firestore';
 
 @Injectable({
   providedIn: 'root'
@@ -6,4 +7,8 @@ import { Injectable } from '@angular/core';
 export class MessageService {
 
   constructor(private db: AngularFirestore) { }
+
+  send() {
+
+  }
 }
