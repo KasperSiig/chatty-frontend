a522b88692067a386cfdae81ef948c8d1a8f7baa
KasperSiig <32297941+KasperSiig@users.noreply.github.com>
2019-04-29 10:30:01 +0200

    Fixed Tests (#5)
    * Added File Structure
    
    * Fixed Tests

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,18 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { HeaderComponent } from './shared/header/header.component';
+import { ChatComponent } from './chat/chat.component';
+import { MessageComponent } from './messages/message/message.component';
+import { SendComponent } from './messages/send/send.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    ChatComponent,
+    MessageComponent,
+    SendComponent
   ],
   imports: [
     BrowserModule,
