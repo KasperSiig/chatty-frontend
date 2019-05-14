e470b24478736dc6a6541361300fa918304dc11c
kaspersiig <kasp9250@easv365.dk>
2019-05-14 09:11:07 +0200

    Fixed Build

--- src/app/messages/send/send.component.spec.ts
+++ src/app/messages/send/send.component.spec.ts
@@ -89,10 +89,6 @@ describe('SendComponent', () => {
     fixture.detectChanges();
     expect(component.isDisabled).toBeTruthy();
   });
-
-  it('should send file to service', () => {
-    
-  });
 });
 
 
