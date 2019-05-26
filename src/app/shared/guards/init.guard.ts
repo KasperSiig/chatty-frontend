6ce8926937d94e35a3eebe2bf01c360898b08c4f
KasperSiig <32297941+KasperSiig@users.noreply.github.com>
2019-05-26 17:09:38 +0200

    Added Small Fixes (#39)
    Files can now be uploaded again. isFile is set to false on all text
    messages. Added InitGuard, so users will be redirected to
    LoadingComponent if FireAuth is not initialised.

---
+++ src/app/shared/guards/init.guard.ts
@@ -0,0 +1,25 @@
+import { Injectable } from '@angular/core';
+import { CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot, UrlTree, Router } from '@angular/router';
+import { Observable } from 'rxjs';
+import { UserService } from '../services/user.service';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class InitGuard implements CanActivate {
+
+  constructor(private userSvc: UserService,
+              private router: Router) {}
+
+  canActivate(
+    next: ActivatedRouteSnapshot,
+    state: RouterStateSnapshot): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
+    if (this.userSvc.authLoaded) {
+      return true;
+    } else {
+      this.router.navigate(['/']);
+      return false;
+    }
+  }
+  
+}
